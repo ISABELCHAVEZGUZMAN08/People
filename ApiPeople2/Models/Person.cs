60a323b25c86ec1d17bc9c6dc42af9328d8cd720
ISABEL <ISABEL@LAPTOP-OL71C2V1>
2021-10-09 07:55:42 -0400

    Agregar archivos de proyecto.

---
+++ ApiPeople2/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeople2.Models
+{
+    public class Person
+    {
+
+        [Key]
+        public int PersonId { get; set; }
+        [Required(ErrorMessage = "El nombre es el requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "El nombre debe contener entre 5 a 60 caracteres")]
+
+        [Display(Name = "nombre completo")]
+
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public Sextype Sex { get; set; }
+
+        [Display(Name = "Pasa tiempos")]
+        public string Hobbie { get; set; }
+    }
+
+}
---
+++ webPeople/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webPeople.Models
+{
+     public enum Sextype
+    {
+        Female=0,
+         Male=1,
+    };
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required(ErrorMessage ="El nombre es el requerido")]
+        [StringLength(60,MinimumLength =5,ErrorMessage ="El nombre debe contener entre 5 a 60 caracteres")]
+       
+        [Display(Name="nombre completo")]
+       
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="Sexo")]
+        public Sextype Sex { get; set; }
+
+        [Display(Name = "Pasa tiempos")]
+        public string Hobbie { get; set; }
+    }
+}
